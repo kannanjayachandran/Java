a0b8ee974af23296590e00cb2b3bcbae8e2c620d
kannan jayachandran <81518270+kannanjayachandran@users.noreply.github.com>
2021-09-19 12:44:06 +0530

    added play again

--- Projects/Number Guessing Game/Main.java
+++ Projects/Number Guessing Game/Main.java
@@ -1,3 +1,5 @@
+package com.company;
+
 
 import java.util.Scanner;
 
@@ -5,32 +7,48 @@ public class Main {
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
-
-        System.out.println("\t\tNUMBER GUESSING GAME");
-        System.out.println("\nGuess a number between 1 and 100\n");
-
-        // Generating a p.random number between 1 and 100.
-        int randomNumber = (int)(Math.random()*100)+1;
-
-        int guess = 0;
+        String playAgain;
         int count =0;
-        while (guess != randomNumber){
 
-            System.out.println(" Enter the number: ");
-            guess = in.nextInt();
-            if (guess > randomNumber){
-                System.out.println(guess + " Too high.. Try again");
-                count++;
-            }
-            else if (guess < randomNumber){
-                System.out.println(guess + " Too low... Try again");
-                count++;
-            }
-            if (guess == randomNumber){
-                System.out.println("\n\nCorrect\tYou won\n \tYou guessed in " + count + " tries");
-            }
-        } //  end of while
-    } // end of main.
+        System.out.println("\t\tNUMBER GUESSING GAME.");
+        System.out.println("\nGuess the numbers between 1 and 100");
+
+        do {
+            // Creating a random number.
+            int randNum = (int) ((Math.random() * 100) + 1);
+            int guessNum = 0;
+
+            while (guessNum != randNum){
+
+                //Asking for input and taking it.
+                System.out.println("\nEnter the number ");
+                guessNum = in.nextInt();
+
+                // Comparing values
+                if (guessNum > randNum){
+                    System.out.println("Your guess " + guessNum + " is too high. Try again..");
+                    count++;
+                }
+                else if (guessNum < randNum){
+                    System.out.println("Your guess " + guessNum + " is too small. Try again...");
+                    count++;
+                }
+                else{
+                    System.out.println("Correct guess.\n Congrats it only took you  " + count + " tries! ");
+                }
+            } // end of inside while loop.
+
+            System.out.println("\tWould you like to play again ?\t(Y/N)");
+            playAgain =in.next();
+        }// end of do.
+
+        while (playAgain.equalsIgnoreCase("y"));
+        {
+            System.out.println("Thank you for playing! Good bye.");
+            in.close();
+
+        } // end of while loop.
+    }
 }
 
 /*  NUMBER GUESSING GAME
@@ -38,4 +56,9 @@ public class Main {
         We will generate a pseudo random number between 1 and 100; and prompt the user to enter a number.
         If the number is less than the generated number, then we will say "too low", if it is higher; then
         "too high".
+
+        *. GENERATING A PSEUDO RANDOM NUMBER.
+           Here Math.random will give a floating point value; we multiply 100 to make it in the range of 100 (0 to 99)
+           (int) will typecast the value to an integer. +1 will change the range.(1 to 100).
+
  */
