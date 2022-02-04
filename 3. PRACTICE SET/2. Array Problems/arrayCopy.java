0d2e80a33174015b84e8b424b4b9503d63d125d7
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-02-04 23:27:58 +0530

    added new practice problem

--- 3. PRACTICE SET/2. Array Problems/arrayCopy.java
+++ 3. PRACTICE SET/2. Array Problems/arrayCopy.java
@@ -1,31 +1,22 @@
-
+// Copying elements of one array to another.
 import java.util.Arrays;
 
 public class arrayCopy {
+    
     public static void main(String[] args) {
 
-        // Copying the elements of an array to another.
-        String []arr = {"Alternative","use" ,"the" ,"shortcut", "syntax",
-                          "to", "create", "and", "initialize", "an", "array"};
-        String []brr = new String[7];
-        System.arraycopy(arr, 3, brr, 0, 7);
+        String []arr = {"one", "two", "three", "four"};
+        String []brr = new String[arr.length];
+
+        System.arraycopy(arr, 0, brr, 0, 4);
+
         System.out.println(Arrays.toString(brr));
     }
 }
 
-
 /*
-    COPYING AN ARRAY : System.arraycopy(name of source array  , src array starting
-                        index(From where we want to start copying) , name of
-                         destination array , dst starting index , Number of elements to copy)
-                  SRC CODE:
-                   String[] arr = {"Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
-                        "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
-                        "Marocchino", "Ristretto" };
-
-        String[] brr = new String[7];
-        System.arraycopy(arr, 2, brr, 0, 7);
-        for (String coffee : brr) {
-            System.out.print(coffee + " ");
-        }
+System.arraycopy : (name of source array, source array starting
+                index(From where we want to start copying),
+                name of destination array , destination starting index,
+                Number of elements to copy)
  */
