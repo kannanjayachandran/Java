1b6de1730c47d58a96779740f727664d7e421cd4
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-03-26 21:09:16 +0530

    added new program

--- 3. PRACTICE SET/1. Basic Programs/primeNumber_1.java
+++ 3. PRACTICE SET/1. Basic Programs/primeNumber_1.java
@@ -1,5 +1,5 @@
 // Find whether a given number is prime or not.
-// Here we use an  optimization; instead of running the loop 'num' times we run it  num/2 times.
+// Here we use an  optimization; instead of running the loop 'n' times we run it  n/2 times.
 // Complexity is O(n/2)
 
 import java.util.Scanner;
@@ -11,22 +11,22 @@ public class primeNumber_1 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the number to check: ");
-        int num =  input.nextInt();
+        int num = input.nextInt();
 
-        if (num == 0 || num == 1) System.out.println(num + " is neither prime nor composite.");
+        if (num == 0 || num == 1)
+            System.out.println(num + " is neither prime nor composite.");
 
-        else
-        {
-            int flag  = 0;
-            for (int i = 2; i < (num/2); i++) {
-                if (num % i == 0)
-                {
+        else {
+            int flag = 0;
+            for (int i = 2; i < (num / 2); i++) {
+                if (num % i == 0) {
                     System.out.println(num + " is not a prime number.");
-                    flag =1;
+                    flag = 1;
                     break;
                 }
             }
-            if (flag == 0) System.out.println(num + " is a prime number.");
-       }
+            if (flag == 0)
+                System.out.println(num + " is a prime number.");
+        }
     }
 }
