0eb2e6779b3b4b58b9639e01f8cd8e70ec5ef61f
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-04-16 12:32:44 +0530

    Added Sieve_prime🌙

---
+++ 3. PRACTICE SET/2. Array Problems/Sieve_eratosthenes.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Sieve_eratosthenes {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int number = input.nextInt();
+
+        erastosthenes(number);
+    } // end of main
+
+    static void erastosthenes(int number) {
+
+        int[] arr = new int[number + 1];
+        arr[1] = 0;
+
+        // Filling the array with one
+        for (int i = 2; i < number; i++) {
+            arr[i] = 1;
+        }
+
+        // finding the multiples and making their value to zero
+        for (int i = 2; i * i <= number; i++) {
+            if (arr[i] == 1) {
+                for (int j = i * i; j <= number; j += i) {
+                    arr[j] = 0;
+
+                }
+            }
+        }
+
+        // Checking how many values are still 1 and printing them
+        for (int i = 1; i <= number; i++) {
+            if (arr[i] == 1)
+                System.out.print(i + " ");
+
+        }
+    }
+}
+
+/*
+ * Java program to find all the prime numbers below a given number, using the
+ * sieve of eratosthenes method.
+ * It can be used for numbers around 10^7.
+ * It is having a time complexity of O(N log (log N))
+ */
