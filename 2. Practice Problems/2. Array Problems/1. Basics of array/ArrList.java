21919701cb70831d587a467d6e29348086b91d4d
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-09-09 12:55:02 +0530

    added new program

---
+++ 2. Practice Problems/2. Array Problems/1. Basics of array/ArrList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ArrList {
+    public static void main(String[] args) {
+
+        // ArrayList or Dynamic arrays
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        // adding elements in arrayList
+        arr.add(100);
+        arr.add(23);
+
+        System.out.println(arr);
+
+        System.out.println(arr.size());
+
+    }
+
+}
